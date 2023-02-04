7915a2472d0c5c8057945075d2c3f66980f80382
olahim <ayankoyaola@gmail.com>
2023-02-03 20:58:07 -0500

    Initial commit of project skeleton

---
+++ billingwoker/src/main/java/com/greenrollsms/billingwoker/BillingwokerApplication.java
@@ -0,0 +1,13 @@
+package com.greenrollsms.billingwoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingwokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingwokerApplication.class, args);
+    }
+
+}
---
+++ billingwoker/src/test/java/com/greenrollsms/billingwoker/BillingwokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenrollsms.billingwoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingwokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
